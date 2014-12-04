8a4fc0377292a889bce5b733fe8e9ed97f9fed28
Arnaud de Villeneuve <arnaudv@mac.com>
2014-12-04 01:02:53 +0100

    GUI v1.0 - Abandoned the idea of reusing the GUI code from the practical, too f*cked up - Restarted the GUI from scratch based on web tutorials: http://www.ntu.edu.sg/home/ehchua/programming/java/JavaGame_TicTacToe.html See 4.3 Sudoku Now we have something that actually works! - Have implemented a refresh() method that should later be called after each iteration to update the puzzle

--- java/ia54/project/taquin/Puzzle.java
+++ java/ia54/project/taquin/Puzzle.java
@@ -1,101 +1,163 @@
 package ia54.project.taquin;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.*;
+ 
+public class Puzzle extends JFrame {
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+// Name-constants for the various dimensions
+   private static int ROWS = 4; // ROWS by COLS cells, default value
+   private static int COLS = 4;
+   // TODO Scale the cell size to the number of tiles
+   private static int CELL_SIZE = 60; // Cell width/height
+   private static int CANVAS_WIDTH = CELL_SIZE * COLS;
+   private static int CANVAS_HEIGHT = CELL_SIZE * ROWS;
+ 
+   // Game board
+   private int[][] cells;
+   private JTextField[][] tfCells;
 
-public class Puzzle {
+ 
+   /** Constructor to setup the game and the GUI */
+   public Puzzle() {
+      Container cp = getContentPane();
+      cp.setLayout(new GridLayout(ROWS, COLS));
+ 
+      ROWS = COLS = SharedValues.getSize();
+      
+      cells = new int[ROWS][COLS];
+      tfCells = new JTextField[ROWS][COLS]; // allocate JTextField array
+ 
+      // Create 9x9 JTextFields and place on the GridLayout
+      for (int row = 0; row < ROWS; ++row) {
+         for (int col = 0; col < COLS; ++col) {
+            tfCells[row][col] = new JTextField(); // allocate element of array
+            cp.add(tfCells[row][col]);  // ContentPane adds JTextField
+            
+            int number = SharedValues.getMatrix(row, col);
+            drawTile(number, row, col);
+         }
+      }
+      
+      cp.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+ 
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      pack();
+      setTitle("Taquin");
+      setVisible(true);
+   }
 
-	private PuzzleFrame puzzleFrame;
-	private int size;
-	private PuzzleSupport puzzleSupport;
-	private int tileWidth;
-	private int tileHeight;	
-	
-	
-	public Puzzle(PuzzleFrame puzzleFrame, int puzzleSize, int puzzleWidth, int puzzleHeight) {
-		super();
-		this.puzzleFrame = puzzleFrame;
-		this.size = puzzleSize;
-		
-		this.tileWidth = puzzleWidth / size;
-		this.tileHeight = puzzleHeight / size;
-		
-		puzzleSupport = new PuzzleSupport(this);
-		Graphics graphics = puzzleSupport.getGraphics();
-		
-		// Draw initial puzzle
-		for (int y = 0 ; y < size ; y++){ 
-			for (int x = 0 ; x < size ; x++){
-				// Try is blank tile
-				if ( SharedValues.getMatrix(x, y) == -1 ){
-					graphics.setColor(Color.WHITE);
-					graphics.fillRect( x*tileWidth, y*tileHeight, x + tileWidth, y + tileHeight );
-				}
-				else{
-					graphics.setColor(Color.GRAY);
-					graphics.fill3DRect( x, y, size, size, true); // Graphical representation of a tile
-					graphics.setColor( Color.WHITE);
-					graphics.setFont( new Font( "Arial", Font.BOLD, size / 2));
-		            FontMetrics fm = graphics.getFontMetrics();
-		            String st = ( new Integer( SharedValues.getMatrix(x, y) )).toString();
-		            graphics.drawString( st, x*tileWidth + ( size - fm.stringWidth(st)) / 2, y*tileHeight + ( size + fm.getAscent()) /2 - 4  );
-				}
+   
+   public void refresh(){
+      for (int row = 0; row < ROWS; ++row) {
+          for (int col = 0; col < COLS; ++col) {
+              int number = SharedValues.getMatrix(row, col);
+              drawTile(number, row, col);
+          }
+      }
+   }
+
+   
+   private void drawTile(int number, int row, int col){
+       // Try if blank tile
+       if ( number == -1 ){
+           tfCells[row][col].setText("");  // empty
+           tfCells[row][col].setBackground(Color.WHITE);
+           tfCells[row][col].setFocusable(false);
+       }
+       else {
+       	// Try if tiles is in correct place		
+			if( SharedValues.getMatrix(row, col) == (row + 4*col)){
+				tfCells[row][col].setBackground(Color.DARK_GRAY);
 			}
-			
-		}		
-	}
-	
-	public void refresh( Graphics graphics){
-		for (int y = 0 ; y < size ; y++){ 
-			for (int x = 0 ; x < size ; x++){
-				// Try is blank tile
-				if ( SharedValues.getMatrix(x, y) == -1 ){
-					graphics.setColor(Color.WHITE);
-					graphics.fillRect( x*tileWidth, y*tileHeight, x + tileWidth, y + tileHeight );
-				}
-				else{
-					// Try if tiles is in correct place		
-					if( SharedValues.getMatrix(x, y) == (x + 4*y)){
-						graphics.setColor(Color.DARK_GRAY);
-					}
-					else{
-						graphics.setColor(Color.GRAY);
-					}
-					graphics.fill3DRect( x, y, size, size, true); // Graphical representation of a tile
-					graphics.setColor( Color.WHITE);
-					graphics.setFont( new Font( "Arial", Font.BOLD, size / 2));
-		            FontMetrics fm = graphics.getFontMetrics();
-		            // In the SharedValues matrix, the values are 0 to 14 and we want to show 1 to 15, thus the +1
-		            String st = ( new Integer( SharedValues.getMatrix(x, y) + 1)).toString();
-		            graphics.drawString( st, x*tileWidth + ( size - fm.stringWidth(st)) / 2, y*tileHeight + ( size + fm.getAscent()) /2 - 4  );
-				}
+			else{
+				tfCells[row][col].setBackground(Color.GRAY);
 			}
-			
-		}	
+           cells[row][col] = number;
+           tfCells[row][col].setText(number + "");
+           tfCells[row][col].setFocusable(false);
+           tfCells[row][col].setHorizontalAlignment(JTextField.CENTER);
+           // TODO Scale the font size to the number of tiles
+           tfCells[row][col].setFont(new Font("Monospaced", Font.BOLD, 20));
+       }
+   }
+   
+   
+   public static int getROWS() {
+		return ROWS;
+   }
+	
+	
+	public static void setROWS(int rOWS) {
+		ROWS = rOWS;
 	}
 	
 	
-	public PuzzleFrame getPuzzleFrame() {
-		return puzzleFrame;
-	}
-	public void setPuzzleFrame(PuzzleFrame puzzleFrame) {
-		this.puzzleFrame = puzzleFrame;
-	}
-	public int getSize() {
-		return size;
-	}
-	public void setSize(int size) {
-		this.size = size;
-	}
-	public PuzzleSupport getPuzzleSupport() {
-		return puzzleSupport;
-	}
-	public void setPuzzleSupport(PuzzleSupport puzzleSupport) {
-		this.puzzleSupport = puzzleSupport;
+	public static int getCOLS() {
+		return COLS;
 	}
 	
 	
+	public static void setCOLS(int cOLS) {
+		COLS = cOLS;
+	}
 	
+	
+	public static int getCELL_SIZE() {
+		return CELL_SIZE;
+	}
+	
+	
+	public static void setCELL_SIZE(int cELL_SIZE) {
+		CELL_SIZE = cELL_SIZE;
+	}
+	
+	
+	public static int getCANVAS_WIDTH() {
+		return CANVAS_WIDTH;
+	}
+	
+	
+	public static void setCANVAS_WIDTH(int cANVAS_WIDTH) {
+		CANVAS_WIDTH = cANVAS_WIDTH;
+	}
+	
+	
+	public static int getCANVAS_HEIGHT() {
+		return CANVAS_HEIGHT;
+	}
+	
+	
+	public static void setCANVAS_HEIGHT(int cANVAS_HEIGHT) {
+		CANVAS_HEIGHT = cANVAS_HEIGHT;
+	}
+	
+	
+	public int[][] getCells() {
+		return cells;
+	}
+	
+	
+	public void setCells(int[][] cells) {
+		this.cells = cells;
+	}
+	
+	
+	public JTextField[][] getTfCells() {
+		return tfCells;
+	}
+	
+	
+	public void setTfCells(JTextField[][] tfCells) {
+		this.tfCells = tfCells;
+	}
+	
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
